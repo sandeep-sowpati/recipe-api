1d02567e2adeb61999ba7f5763ad7d8a26fb264b
sandeep-sowpati <letsmailsandeepsowpati@gmail.com>
2023-01-13 23:57:02 +0530

    fixed identation

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,6 +1,8 @@
-""" 
+"""
 Calculator functions
 """
 
+
 def add(x, y):
+
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,8 +6,11 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    
+
     def test_add_numbers(self):
+
         res = calc.add(5, 6)
+
         self.assertEqual(res, 11)
